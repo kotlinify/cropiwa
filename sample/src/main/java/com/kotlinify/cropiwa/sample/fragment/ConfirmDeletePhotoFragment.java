85ce682025190327f066898bdf2bfc268c6b8116
Umut ADALI <kobeumut@gmail.com>
2019-08-14 15:47:10 +0300

    [chore] Update package folder

---
+++ sample/src/main/java/com/kotlinify/cropiwa/sample/fragment/ConfirmDeletePhotoFragment.java
@@ -0,0 +1,51 @@
+package com.kotlinify.cropiwa.sample.fragment;
+
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kotlinify.cropiwa.sample.R;
+
+/**
+ * Created by yarolegovich https://github.com/yarolegovich
+ * on 22.03.2017.
+ */
+public class ConfirmDeletePhotoFragment extends BottomSheetDialogFragment implements View.OnClickListener {
+
+    private Uri image;
+    private Listener listener;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_confirm_delete, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        view.findViewById(R.id.btn_delete).setOnClickListener(this);
+    }
+
+    public void setListener(Listener listener, Uri image) {
+        this.listener = listener;
+        this.image = image;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_delete) {
+            if (listener != null) {
+                listener.onDeleteConfirmed(image);
+            }
+            dismiss();
+        }
+    }
+
+    public interface Listener {
+        void onDeleteConfirmed(Uri image);
+    }
+}
---
+++ sample/src/main/java/com/kotlinify/cropiwa/sample/util/Permissions.java
@@ -0,0 +1,19 @@
+package com.kotlinify.cropiwa.sample.util;
+
+import android.content.pm.PackageManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+/**
+ * Created by yarolegovich https://github.com/yarolegovich
+ * on 22.03.2017.
+ */
+
+public class Permissions {
+
+    public static boolean isGranted(AppCompatActivity activity, String permission) {
+        return ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+}
